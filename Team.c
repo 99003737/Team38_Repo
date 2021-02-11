b601778fd92b2ac75ad967ba2b78595c410f1271
99003746 <nitin.shetty1@ltts.com>
2021-02-11 12:13:18 +0530

    some modifiction has been done.

--- Team.c
+++ Team.c
@@ -2,10 +2,6 @@
 int main()
 {
     printf("Hello World,this is jeshwanth");
-<<<<<<< HEAD
     printf("\nhello World this is Nitin");
-=======
-    printf("hello world ,this is priya");
->>>>>>> 7d889c7a08cfedef2234c9ea5abcfde82d84f2f5
     return 0;
 }
